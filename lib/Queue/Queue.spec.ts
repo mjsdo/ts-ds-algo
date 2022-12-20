f75ad6fb1eb10fb02ff93b91d4e6f1a21c544f9f
Cola <bcad1591@gmail.com>
2022-12-21 07:32:55 +0900

    feat: Queue

---
+++ lib/Queue/Queue.spec.ts
@@ -0,0 +1,55 @@
+import Queue from './Queue';
+
+describe('Queue', () => {
+  let queue: Queue<number>;
+
+  beforeEach(() => {
+    queue = new Queue();
+  });
+
+  it('초기화시 head와 tail은 null이다.', () => {
+    expect(queue.head).toBeNull();
+    expect(queue.tail).toBeNull();
+  });
+
+  it('초기화시 isEmpty()가 true이다.', () => {
+    expect(queue.isEmpty()).toBe(true);
+  });
+
+  it('enqueue하면 tail에 추가된다.', () => {
+    queue.enqueue(1);
+    expect(queue.back()).toBe(1);
+
+    queue.enqueue(2).enqueue(3);
+    expect(queue.back()).toBe(3);
+
+    expect(queue.isEmpty()).toBe(false);
+  });
+
+  it('아무것도 없을 때 dequeue하면 undefined가 반환된다.', () => {
+    expect(queue.dequeue()).toBeUndefined();
+  });
+
+  it('요소가 있을 때 dequeue하면 head.value가 반환된다.', () => {
+    queue.enqueue(1).enqueue(2);
+
+    let headValue = queue.front();
+    const dequeue1Value = queue.dequeue();
+
+    expect(dequeue1Value).toBe(headValue);
+
+    headValue = queue.front();
+
+    const dequeue2Value = queue.dequeue();
+
+    expect(dequeue2Value).toBe(headValue);
+    expect(queue.isEmpty()).toBe(true);
+  });
+
+  it('peek하면 head.value가 반환된다.', () => {
+    expect(queue.peek()).toBeUndefined();
+
+    queue.enqueue(1);
+    expect(queue.peek()).toBe(1);
+  });
+});
---
+++ lib/Queue/Queue.ts
@@ -0,0 +1,61 @@
+import type { Nullable } from 'lib/utils/types';
+
+export class Node<T> {
+  // eslint-disable-next-line no-use-before-define
+  next: Nullable<Node<T>> = null;
+  value: T;
+
+  constructor(value: T) {
+    this.value = value;
+  }
+}
+
+export default class Queue<T> {
+  head: Nullable<Node<T>> = null;
+  tail: Nullable<Node<T>> = null;
+  size = 0;
+
+  isEmpty() {
+    return this.head === null;
+  }
+
+  /** `head.value` */
+  peek() {
+    return this.head?.value;
+  }
+
+  /** `head.value` */
+  front() {
+    return this.head?.value;
+  }
+
+  /** `tail.value` */
+  back() {
+    return this.tail?.value;
+  }
+
+  enqueue(value: T) {
+    const newNode = new Node(value);
+
+    this.size += 1;
+    if (this.isEmpty()) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail!.next = newNode;
+      this.tail = newNode;
+    }
+
+    return this;
+  }
+
+  dequeue() {
+    if (!this.head) return undefined;
+
+    const dequeueValue = this.head.value;
+
+    this.head = this.head.next;
+    this.size -= 1;
+    return dequeueValue;
+  }
+}
