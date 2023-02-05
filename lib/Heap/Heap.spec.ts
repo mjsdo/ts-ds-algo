71479975d5f36bbb4061012673697c6056681222
Cola <bcad1591@gmail.com>
2023-02-05 22:05:20 +0900

    refactor: Heap
    - heapify 추가
    - siftDown, siftUp메서드를 재귀 방식으로 변경

--- lib/Heap/Heap.spec.ts
+++ lib/Heap/Heap.spec.ts
@@ -1,4 +1,4 @@
-import { MaxHeap, MinHeap, HeapInterface, createHeap } from './Heap';
+import { HeapInterface, default as Heap } from './Heap';
 interface Variant {
   v1: number;
   v2: number;
@@ -7,14 +7,14 @@ interface Variant {
 describe('Heap', () => {
   let heap: HeapInterface<number>;
   beforeEach(() => {
-    heap = new MaxHeap();
+    heap = new Heap((a, b) => a - b);
   });
 
   it('초기화하면 size가 0이다.', () => {
     expect(heap.size()).toBe(0);
   });
 
-  it('push하면 size가 1 늘어난다.', () => {
+  it('push로 배열이 아닌 값을 전달하면 size가 1 늘어난다.', () => {
     heap.push(1);
     expect(heap.size()).toBe(1);
     heap.push(1);
@@ -23,6 +23,12 @@ describe('Heap', () => {
     expect(heap.size()).toBe(3);
     heap.push(1);
     expect(heap.size()).toBe(4);
+  });
+
+  it('push로 배열을 전달하면 전달한 배열의 길이만큼 size가 늘어난다', () => {
+    const arr = [1, 2, 3, 4];
+    heap.push(arr);
+    expect(heap.size()).toBe(arr.length);
   });
 
   it('pop하면 size가 1 줄어든다.', () => {
@@ -43,94 +49,119 @@ describe('Heap', () => {
     expect(heap.size()).toBe(0);
   });
 
-  describe('MaxHeap', () => {
-    let maxHeap: HeapInterface<number>;
-    beforeEach(() => {
-      maxHeap = new MaxHeap();
-      maxHeap
-        .push(23)
-        .push(9)
-        .push(8)
-        .push(100)
-        .push(-100)
-        .push(34)
-        .push(51)
-        .push(-50)
-        .push(-64);
-    });
+  it('toArray하면 null값이 없는 배열로 변환후 반환한다.', () => {
+    const nums = [1, 2, 3, 4];
+    const isNull = (v: any) => v === null;
 
-    it('pop하면 크기가 가장 큰 숫자가 제거된다.', () => {
-      const arr = [];
-      while (maxHeap.size()) arr.push(maxHeap.pop());
-      const dup = ([...arr] as number[]).sort((a, b) => b - a);
-
-      expect(arr).toEqual(dup);
-    });
-
-    it('peek하면 크기가 가장 큰 숫자가 반환된다.', () => {
-      while (maxHeap.size()) expect(maxHeap.peek()).toBe(maxHeap.pop());
-    });
+    heap.push(nums);
+    const arr = heap.toArray();
+    expect(arr.some(isNull)).toBeFalsy();
   });
 
-  describe('MinHeap', () => {
-    let minHeap: HeapInterface<number>;
-    beforeEach(() => {
-      minHeap = new MinHeap();
-      minHeap
-        .push(23)
-        .push(9)
-        .push(8)
-        .push(100)
-        .push(-100)
-        .push(34)
-        .push(51)
-        .push(-50)
-        .push(-64);
-    });
+  it('toArray해서 반환받은 배열에는 heap이 가지고 있던 모든 요소가 있다.', () => {
+    const nums = [4, 3, 2, 1, 0, 1, 2, 3, 4];
+    heap.push(nums);
+    const arr = heap.toArray();
+    while (nums.length) {
+      const pop = nums.pop() as number;
+      expect(arr.includes(pop)).toBeTruthy();
+    }
+  });
+});
 
-    it('pop하면 크기가 가장 작은 숫자가 제거된다.', () => {
-      const arr = [];
-      while (minHeap.size()) arr.push(minHeap.pop());
-      const dup = ([...arr] as number[]).sort((a, b) => a - b);
+describe('MaxHeap', () => {
+  let maxHeap: HeapInterface<number>;
+  const maxHeapCompareFn = (a: number, b: number) => b - a;
+  const nums = [23, 9, 8, 100, -100, 34, 51, -50, -64];
+  const numsSortedByMaxHeapCompareFn = [...nums].sort(maxHeapCompareFn);
 
-      expect(arr).toEqual(dup);
-    });
-
-    it('peek하면 크기가 가장 작은 숫자가 반환된다.', () => {
-      while (minHeap.size()) expect(minHeap.peek()).toBe(minHeap.pop());
-    });
+  beforeEach(() => {
+    maxHeap = new Heap(maxHeapCompareFn);
+    maxHeap.push(nums);
   });
 
-  describe('Variant', () => {
-    let heap: HeapInterface<Variant>;
-    // v1 오름차순, v1이 같다면 v2 내림차순
-    const sortFn = (a: Variant, b: Variant) => {
-      if (a.v1 === b.v1) {
-        return b.v2 - a.v2;
-      }
-      return a.v1 - b.v1;
-    };
+  it('pop하면 크기가 가장 큰 숫자가 제거된다.', () => {
+    const pops = [];
+    while (maxHeap.size()) pops.push(maxHeap.pop());
+    expect(pops).toEqual(numsSortedByMaxHeapCompareFn);
+  });
 
-    beforeEach(() => {
-      const Heap = createHeap<Variant>(sortFn);
-      heap = new Heap();
-      heap
-        .push({ v1: 10, v2: 0 })
-        .push({ v1: -30, v2: 10 })
-        .push({ v1: -40, v2: -40 })
-        .push({ v1: 3, v2: 9 })
-        .push({ v1: 7, v2: 4 })
-        .push({ v1: 3, v2: 11 })
-        .push({ v1: 1, v2: -30 })
-        .push({ v1: 0, v2: 21 })
-        .push({ v1: -40, v2: -40 });
-    });
+  it('peek하면 크기가 가장 큰 숫자가 반환된다.', () => {
+    while (maxHeap.size()) expect(maxHeap.peek()).toBe(maxHeap.pop());
+  });
+});
 
-    it('pop순서는 정렬 함수의 순서와 일치한다.', () => {
-      const arr = [];
-      while (heap.size()) arr.push(heap.pop());
-      const dup = ([...arr] as Variant[]).sort(sortFn);
-      expect(arr).toEqual(dup);
-    });
+describe('MinHeap', () => {
+  let minHeap: HeapInterface<number>;
+  const minHeapCompareFn = (a: number, b: number) => a - b;
+  const nums = [23, 9, 8, 100, -100, 34, 51, -50, -64];
+  const numsSortedByMinHeapCompareFn = [...nums].sort(minHeapCompareFn);
+
+  beforeEach(() => {
+    minHeap = new Heap(minHeapCompareFn);
+    minHeap.push(nums);
+  });
+
+  it('pop하면 크기가 가장 작은 숫자가 제거된다.', () => {
+    const pops = [];
+    while (minHeap.size()) pops.push(minHeap.pop());
+    expect(pops).toEqual(numsSortedByMinHeapCompareFn);
+  });
+
+  it('peek하면 크기가 가장 작은 숫자가 반환된다.', () => {
+    while (minHeap.size()) expect(minHeap.peek()).toBe(minHeap.pop());
+  });
+});
+
+describe('Variant', () => {
+  let heap: HeapInterface<Variant>;
+  // v1 오름차순, v1이 같다면 v2 내림차순
+  const sortFn = (a: Variant, b: Variant) => {
+    if (a.v1 === b.v1) {
+      return b.v2 - a.v2;
+    }
+    return a.v1 - b.v1;
+  };
+
+  const vals = [
+    { v1: 10, v2: 0 },
+    { v1: -30, v2: 10 },
+    { v1: -40, v2: -40 },
+    { v1: 3, v2: 9 },
+    { v1: 7, v2: 4 },
+    { v1: 3, v2: 11 },
+    { v1: 1, v2: -30 },
+    { v1: 0, v2: 21 },
+    { v1: -40, v2: -40 },
+  ];
+
+  const valsSortedBySortFn = [...vals].sort(sortFn);
+
+  beforeEach(() => {
+    heap = new Heap(sortFn);
+    heap.push(vals);
+  });
+
+  it('pop순서는 정렬 함수로 heap배열을 정렬한 순서와 일치한다.', () => {
+    const pops = [];
+    while (heap.size()) pops.push(heap.pop());
+    expect(pops).toEqual(valsSortedBySortFn);
+  });
+});
+
+describe('Heapify', () => {
+  const arr = [1, 2, 3, 4, 4, 4, 5, 6, 6, 7, 8, 9];
+  const compareFn = (a: number, b: number) => b - a;
+  const sorted = [...arr].sort(compareFn);
+  let maxHeap: HeapInterface<number>;
+
+  beforeEach(() => {
+    maxHeap = Heap.heapify(arr, compareFn);
+  });
+
+  it('Heapify로 생성한 heap의 pop 순서는 정렬 함수로 heap배열을 정렬한 순서와 일치한다.', () => {
+    const pops = [];
+    while (maxHeap.size()) pops.push(maxHeap.pop());
+    expect(sorted).toEqual(pops);
   });
 });
--- lib/Heap/Heap.ts
+++ lib/Heap/Heap.ts
@@ -6,157 +6,147 @@
 export type CompareFn<T> = (a: T, b: T) => number;
 
 export interface HeapInterface<T> {
-  heap: [null, ...T[]];
-  compareFn: (a: T | undefined, b: T | undefined) => ReturnType<CompareFn<T>>;
-  push: (value: T) => this;
+  compareFn: (a: T, b: T) => ReturnType<CompareFn<T>>;
+  push: (value: T | T[]) => this;
   peek: () => T | undefined;
   pop: () => T | undefined;
   size: () => number;
   isEmpty: () => boolean;
   clear: () => void;
+  toArray: () => T[];
 }
+export default class Heap<T> implements HeapInterface<T> {
+  private heap: [null, ...T[]] = [null];
+  compareFn: CompareFn<T>;
 
-export const createHeap = <T>(compareFn: CompareFn<T>) =>
-  class Heap implements HeapInterface<T> {
-    heap: [null, ...T[]] = [null];
+  constructor(compareFn: CompareFn<T>) {
+    this.compareFn = compareFn;
+  }
 
-    compareFn(a: T | undefined, b: T | undefined) {
-      if (a !== undefined && b !== undefined) return compareFn(a, b);
-      if (a !== undefined && b === undefined) return -1;
-      if (a === undefined && b !== undefined) return 1;
-      return 0;
-    }
+  isEmpty() {
+    return !this.size();
+  }
 
-    isEmpty() {
-      return !this.size();
-    }
+  size() {
+    return this.heap.length - 1;
+  }
 
-    size() {
-      return this.heap.length - 1;
-    }
-
-    push(value: T) {
-      let targetIndex = this.size() + 1;
-      this.heap[targetIndex] = value;
-      this.siftUp(targetIndex);
+  push(value: T | T[]) {
+    if (Array.isArray(value)) {
+      value.forEach((v) => this.push(v));
       return this;
     }
 
-    pop() {
-      const size = this.size();
-      if (size === 0) return;
-      if (size === 1) return this.heap.pop() as T;
+    this.heap.push(value);
+    this.siftUp(this.size());
+    return this;
+  }
 
-      const removedValue = this.heap[1];
-      this.heap[1] = this.heap.pop() as T;
-      this.siftDown(1);
+  pop() {
+    if (this.size() === 0) return;
+    if (this.size() === 1) return this.heap.pop() as T;
 
-      return removedValue;
+    const removedValue = this.peek();
+    this.heap[1] = this.heap.pop() as T;
+    this.siftDown(1);
+
+    return removedValue;
+  }
+
+  peek() {
+    return this.heap[1];
+  }
+
+  clear() {
+    this.heap = [null];
+  }
+
+  private getParentIndex(childIndex: number) {
+    return Math.floor(childIndex / 2);
+  }
+
+  private getLeftChildIndex(parentIndex: number) {
+    return parentIndex * 2;
+  }
+
+  private getRightChildIndex(parentIndex: number) {
+    return parentIndex * 2 + 1;
+  }
+
+  private getChildrenIndex(parentIndex: number) {
+    if (parentIndex <= 0)
+      throw new Error(
+        'getChildrenIndex Error: parentIndex는 1 이상이어야 합니다.',
+      );
+
+    return [
+      this.getLeftChildIndex(parentIndex),
+      this.getRightChildIndex(parentIndex),
+    ] as const;
+  }
+
+  private swap(index1: number, index2: number) {
+    const { heap } = this;
+    [heap[index1], heap[index2]] = [heap[index2], heap[index1]];
+  }
+
+  private siftUp(childIndex: number) {
+    if (childIndex <= 1 || childIndex > this.size()) return;
+
+    const parentIndex = this.getParentIndex(childIndex);
+    const result = this.compareFn(
+      this.heap[childIndex] as T,
+      this.heap[parentIndex] as T,
+    );
+
+    result < 0 &&
+      (this.swap(childIndex, parentIndex), this.siftUp(parentIndex));
+  }
+
+  private siftDown(parentIndex: number) {
+    if (parentIndex < 1 || parentIndex > this.size() / 2) return;
+    const { heap, compareFn } = this;
+
+    const [leftIndex, rightIndex] = this.getChildrenIndex(parentIndex);
+
+    let result;
+    let selectedIndex;
+
+    if (rightIndex > this.size()) {
+      selectedIndex = leftIndex;
+      result = compareFn(heap[parentIndex] as T, heap[leftIndex] as T);
+    } else {
+      result =
+        compareFn(heap[leftIndex] as T, heap[rightIndex] as T) < 0
+          ? ((selectedIndex = leftIndex),
+            compareFn(heap[parentIndex] as T, heap[leftIndex] as T))
+          : ((selectedIndex = rightIndex),
+            compareFn(heap[parentIndex] as T, heap[rightIndex] as T));
     }
 
-    peek() {
-      return this.isEmpty() ? undefined : this.heap[1];
-    }
+    result > 0 &&
+      (this.swap(parentIndex, selectedIndex), this.siftDown(selectedIndex));
+  }
 
-    clear() {
-      this.heap = [null];
-    }
+  /**
+   * @param arr heap구조로 변환할 배열
+   * @param compareFn 비교 함수
+   * @description Bottom-up heap construction
+   */
+  static heapify<V>(arr: V[], compareFn: CompareFn<V>) {
+    const { length } = arr;
+    const heap = new Heap<V>(compareFn);
+    heap.heap = [null, ...arr];
 
-    private getParentIndex(childIndex: number) {
-      return Math.floor(childIndex / 2);
-    }
+    let index = Math.floor(length / 2);
+    while (index > 0) heap.siftDown(index--);
+    return heap;
+  }
 
-    private getLeftChildIndex(parentIndex: number) {
-      return parentIndex * 2;
-    }
+  toArray() {
+    return this.heap.slice(1) as T[];
+  }
+}
 
-    private getRightChildIndex(parentIndex: number) {
-      return parentIndex * 2 + 1;
-    }
-
-    private getChildrenIndex(parentIndex: number) {
-      if (parentIndex <= 0)
-        throw new Error(
-          'getChildrenIndex Error: parentIndex는 1 이상이어야 합니다.',
-        );
-
-      return [
-        this.getLeftChildIndex(parentIndex),
-        this.getRightChildIndex(parentIndex),
-      ] as const;
-    }
-
-    private getChildren(parentIndex: number) {
-      const [l, r] = this.getChildrenIndex(parentIndex);
-      return [this.heap[l], this.heap[r]] as
-        | [T, T]
-        | [T, undefined]
-        | [undefined, undefined];
-    }
-
-    private getChildrenIndexAndElement(parentIndex: number) {
-      return [
-        ...this.getChildrenIndex(parentIndex),
-        ...this.getChildren(parentIndex),
-      ] as const;
-    }
-
-    private swap(index1: number, index2: number) {
-      const { heap } = this;
-      [heap[index1], heap[index2]] = [heap[index2], heap[index1]];
-    }
-
-    private siftUp(childIndex: number) {
-      if (childIndex <= 0 || childIndex > this.size()) {
-        throw new Error(
-          `siftUp Error: index 범위 초과입니다.\n현재 heap 크기: ${this.size()}\n요청된 index: ${childIndex}`,
-        );
-      }
-
-      const value = this.heap[childIndex] as T;
-      let parentIndex = this.getParentIndex(childIndex);
-
-      while (
-        parentIndex > 0 &&
-        this.compareFn(this.heap[parentIndex] as T, value) > 0
-      ) {
-        this.swap(parentIndex, childIndex);
-        childIndex = parentIndex;
-        parentIndex = this.getParentIndex(parentIndex);
-      }
-    }
-
-    private siftDown(parentIndex: number = 1) {
-      if (parentIndex <= 0 || parentIndex > this.size()) {
-        throw new Error(
-          `siftDown Error: index 범위 초과입니다.\n현재 heap 크기: ${this.size()}\n요청된 index: ${parentIndex}`,
-        );
-      }
-
-      const value = this.heap[parentIndex] as T;
-
-      let [leftChildIndex, rightChildIndex, leftChild, rightChild] =
-        this.getChildrenIndexAndElement(parentIndex);
-      let selectedIndex;
-
-      while (
-        (leftChild !== undefined && this.compareFn(value, leftChild) > 0) ||
-        (rightChild !== undefined && this.compareFn(value, rightChild) > 0)
-      ) {
-        selectedIndex =
-          this.compareFn(leftChild, rightChild) <= 0
-            ? leftChildIndex
-            : rightChildIndex;
-        this.swap(parentIndex, selectedIndex);
-
-        parentIndex = selectedIndex;
-        [leftChildIndex, rightChildIndex, leftChild, rightChild] =
-          this.getChildrenIndexAndElement(parentIndex);
-      }
-    }
-
-    heapify() {}
-  };
-
-export const MaxHeap = createHeap<number>((a: number, b: number) => b - a);
-export const MinHeap = createHeap<number>((a: number, b: number) => a - b);
+export const maxHeap = new Heap<number>((a: number, b: number) => b - a);
+export const minHeap = new Heap<number>((a: number, b: number) => a - b);
